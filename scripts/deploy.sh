7f7f786c71875843edd5cc3bebb0a1dc0256c19e
kitaee <dlrlxo999@naver.com>
2022-08-19 23:53:05 +0900

    ⚡️ [IMPROVE] : Code Deploy 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,16 @@ REPOSITORY=/home/ubuntu/app/build/libs
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(lsof -ti tcp:8080)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID" ]; then
+if [ -z "${CURRENT_PID}" ]
+then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -9 $CURRENT_PID"
+  sudo kill -9 "$CURRENT_PID"
   sleep 5
 fi
 
