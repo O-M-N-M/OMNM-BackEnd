5785ffbf83834738bd039114dad28170a0a78dd6
kitaee <dlrlxo999@naver.com>
2022-08-19 23:32:06 +0900

    ⚡️ [IMPROVE] : Code Deploy 테스트

--- src/main/java/OMNM/OMNMBACKEND/OmnmBackendApplication.java
+++ src/main/java/OMNM/OMNMBACKEND/OmnmBackendApplication.java
@@ -8,7 +8,7 @@ public class OmnmBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OmnmBackendApplication.class, args);
-		System.out.println("Hello World!");
+		System.out.println("kitae is best");
 	}
 
 }
