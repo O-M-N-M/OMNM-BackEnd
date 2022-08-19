a7417b74eec2fb2bbb7fde94444e0cd3b1e431b8
kitaee <dlrlxo999@naver.com>
2022-08-19 23:17:45 +0900

    ⚡️ [IMPROVE] : Code Deploy 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,6 +29,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 #nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
-nohup java -jar -Dspring.config.location=classpath:/,/home/ubuntu/app/application-mysql.properties -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:application-mysql.properties -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
 
 echo "> 기태 짱"
