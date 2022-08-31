5ce4fb93360a1675b596b9b78084fafae06404ca
kitaee <dlrlxo999@naver.com>
2022-08-31 16:40:35 +0900

    ✨ [FEAT] : 메인페이지 룸메이트 신청 API 구현

---
+++ src/main/java/OMNM/OMNMBACKEND/main/service/MainService.java
@@ -0,0 +1,29 @@
+package OMNM.OMNMBACKEND.main.service;
+
+import OMNM.OMNMBACKEND.connection.domain.Connection;
+import OMNM.OMNMBACKEND.connection.repository.ConnectionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MainService {
+
+    private final ConnectionRepository connectionRepository;
+
+    public boolean isProposedPerson(Long userId, Long matchingId){
+        if(connectionRepository.findByFromIdAndToId(userId, matchingId).isPresent()){
+            return true;
+        }
+        else{
+            Connection connection = new Connection();
+            connection.setFromId(userId);
+            connection.setToId(matchingId);
+            connectionRepository.save(connection);
+            return false;
+        }
+    }
+
+}
