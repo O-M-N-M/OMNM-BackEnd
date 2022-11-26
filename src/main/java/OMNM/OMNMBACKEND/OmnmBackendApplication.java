68160051a2bdc89f3267070e4258d6e88fde5145
이기태 <91439717+kitaee@users.noreply.github.com>
2022-11-26 19:35:11 +0900

    Update OmnmBackendApplication.java

--- src/main/java/OMNM/OMNMBACKEND/OmnmBackendApplication.java
+++ src/main/java/OMNM/OMNMBACKEND/OmnmBackendApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//1
 public class OmnmBackendApplication {
 
 	public static void main(String[] args) {
