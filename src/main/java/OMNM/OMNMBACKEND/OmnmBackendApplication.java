ee14a4c36983eb61832634472e3b7ed51ed5139c
SL313 <rebin0313@gmail.com>
2022-12-26 13:23:55 +0900

    :ambulance:[HOTFIX] : fix security

--- src/main/java/OMNM/OMNMBACKEND/OmnmBackendApplication.java
+++ src/main/java/OMNM/OMNMBACKEND/OmnmBackendApplication.java
@@ -2,6 +2,9 @@ package OMNM.OMNMBACKEND;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @SpringBootApplication
 //12
@@ -9,6 +12,13 @@ public class OmnmBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OmnmBackendApplication.class, args);
+	}
+
+	@Bean
+	public HttpFirewall allowUrlSemicolonHttpFirewall(){
+		StrictHttpFirewall firewall = new StrictHttpFirewall();
+		firewall.setAllowSemicolon(true);
+		return firewall;
 	}
 
 }
