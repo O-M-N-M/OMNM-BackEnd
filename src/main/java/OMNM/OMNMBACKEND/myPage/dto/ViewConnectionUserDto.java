cb67577b637ec07e44593dbd3b51dfe648481d4a
SL313 <rebin0313@gmail.com>
2023-01-05 16:48:41 +0900

    :sparkles: [FEAT] : 마이페이지 더보기 페이징처리 API 구현

---
+++ src/main/java/OMNM/OMNMBACKEND/myPage/dto/ViewConnectionUserDto.java
@@ -0,0 +1,15 @@
+package OMNM.OMNMBACKEND.myPage.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ViewConnectionUserDto {
+    private Long userId;
+    private String profileUrl;
+    private String name;
+    private Integer age;
+    private String time;
+    private String mbti;
+    private boolean isLast;
+}
